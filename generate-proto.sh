b0ff034b457663d838be4706872f520962fbd9b4
Goheeca <goheeca@gmail.com>
2020-06-19 21:24:19 +0200

    Protobuf shell script

---
+++ generate-proto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+OUT_DIR=./protobuf
+
+#python -m pip install grpcio
+#python -m pip install grpcio-tools
+
+python -m grpc_tools.protoc -I. --python_out=$OUT_DIR --grpc_python_out=$OUT_DIR engine.proto
