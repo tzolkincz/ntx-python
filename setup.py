9ceb07ebe99faded87987d1d36f4a0b62dfd81ca
Goheeca <goheeca@gmail.com>
2020-07-16 17:29:10 +0200

    Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ntx-python",
-    version="1.0.5",
+    version="1.1.1",
     author="Goheeca",
     author_email="goheeca@gmail.com",
     description="Python library for speech to text using NanoTrix cloud",
